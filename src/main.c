ce5db76c97af1bcda4a14087c3de5c50f16e20ec
Marco Stundner <marco.ms553@gmail.com>
2022-06-27 16:28:42 +0000

    callbacks fuer diff und problem bei sudoku gen

--- src/main.c
+++ src/main.c
@@ -10,6 +10,7 @@
 
 #include <gtk/gtk.h>
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
@@ -17,6 +18,9 @@
 
 #define SudokuX N-1
 #define SudokuY N-1
+#define HARD 60
+#define NORMAL 40
+#define EASY 20
 
 struct widget
 {
@@ -35,21 +39,85 @@ struct widget
     GtkWidget *easy_m;
     GtkWidget *difficultymenu;
     GtkApplication *app;
+    GtkWidget *statusbar;
+    guint id;
+    int solution[N*N];
+    int puzzle[N*N];
+    bool hard;
+    bool normal;
+    bool easy;
 };
 
 #define ent(IDX,IDY) \
 	w->entry[IDX] = gtk_entry_new(); \
 	gtk_widget_set_size_request(w->entry[IDX],50, 50); \
-    gtk_entry_set_width_chars(w->entry[IDX],4);\
-    gtk_entry_set_alignment(w->entry[IDX],0.5);\
+    gtk_entry_set_width_chars(GTK_ENTRY(w->entry[IDX]),4);\
+    gtk_entry_set_alignment(GTK_ENTRY(w->entry[IDX]),0.5);\
     gtk_grid_attach(GTK_GRID(w->grid),w->entry[IDX],IDX,IDY,1,1);
 
 
-void quit_callback (GSimpleAction *action, GVariant *parameter, gpointer user_data)
+static void quit_callback (GSimpleAction *action, GVariant *parameter, gpointer user_data)
 {
 	struct widget *w = (struct widget *)user_data;
 	g_application_quit (G_APPLICATION (w->app));
 }
+
+
+static void difficulty_callback_hard (GtkWidget *widget, gpointer user_data)
+{
+	struct widget *w = (struct widget *)user_data;
+    w->hard = true;
+    w->normal = false;
+    w->easy = false;
+    printf("Difficulty: Hard %d\n",w->hard);
+}
+
+static void difficulty_callback_normal (GtkWidget *widget, gpointer user_data)
+{
+	struct widget *w = (struct widget *)user_data;
+    w->hard = false;
+    w->normal = true;
+    w->easy = false;
+    printf("Difficulty: Normal %d\n",w->normal);
+}
+
+static void difficulty_callback_easy (GtkWidget *widget, gpointer user_data)
+{
+	struct widget *w = (struct widget *)user_data;
+    w->hard = false;
+    w->normal = false;
+    w->easy = true;
+    printf("Difficulty: Easy %d\n",w->easy);
+}
+
+static void sudoku_build (GtkWidget *widget, gpointer user_data)
+{
+    struct widget *w = (struct widget *)user_data;  
+    char buffer[2]; 
+    sudoku_generate(w->solution);
+    memcpy(w->puzzle,w->solution,sizeof(w->puzzle));
+
+    if(w->hard == true){sudoku_classic(w->puzzle,HARD);}
+    if(w->normal == true){sudoku_classic(w->puzzle,NORMAL);}
+    if(w->easy == true){sudoku_classic(w->puzzle,EASY);}
+
+    for(int i = 0; i <= N*N; i++)
+    {
+        buffer[0] = *(g_ascii_dtostr(buffer,2,(double) w->puzzle[i]));   
+        const char *buffer_entry = buffer;
+        printf("%i buffer_entry: %s\n",i,buffer_entry);
+        printf("%i buffer: %s\n",i,buffer);
+        printf("%i puzzle: %d\n\n",i,w->puzzle[i]);
+
+        gtk_entry_set_text(GTK_ENTRY(w->entry[i]),buffer_entry);
+        if(w->puzzle[i] == 0)
+        {
+            gtk_entry_set_text(GTK_ENTRY(w->entry[i]),"");   
+        }  
+    }
+
+}
+
 
 
 static void activate (GtkApplication *app, gpointer user_data)
@@ -63,7 +131,7 @@ static void activate (GtkApplication *app, gpointer user_data)
 	gtk_window_set_title (GTK_WINDOW (window), "Sudoku");
     gtk_window_set_resizable(GTK_WINDOW(window), FALSE);
 
-    w->vbox = gtk_vbox_new(FALSE, 0);
+    w->vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
     gtk_container_add(GTK_CONTAINER(window), w->vbox);
 
     //MENU@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
@@ -92,8 +160,14 @@ static void activate (GtkApplication *app, gpointer user_data)
     gtk_box_pack_start(GTK_BOX(w->vbox), w->menubar, FALSE, FALSE, 0);
 
     g_signal_connect(G_OBJECT(w->quit_m),"activate",G_CALLBACK(quit_callback), NULL);
+    g_signal_connect(G_OBJECT(w->restart_m),"activate",G_CALLBACK(sudoku_build), w);
+    g_signal_connect(G_OBJECT(w->hard_m),"activate",G_CALLBACK(difficulty_callback_hard), w);
+    g_signal_connect(G_OBJECT(w->normal_m),"activate",G_CALLBACK(difficulty_callback_normal), w);
+    g_signal_connect(G_OBJECT(w->easy_m),"activate",G_CALLBACK(difficulty_callback_easy), w);
     //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
+
+    //ENTRYS@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
     w->grid = gtk_grid_new();
     gtk_box_pack_start(GTK_BOX(w->vbox),w->grid, TRUE, FALSE, 0);
     
@@ -105,6 +179,17 @@ static void activate (GtkApplication *app, gpointer user_data)
             ent(i,j);
         }
     }
+    //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+
+    //STATUSBAR@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@    
+    w->statusbar = gtk_statusbar_new();
+    gtk_widget_set_size_request (w->statusbar, 300, 10);
+    gtk_box_pack_start (GTK_BOX (w->vbox), w->statusbar, FALSE, FALSE, 0);
+    w->id = gtk_statusbar_get_context_id (GTK_STATUSBAR (w->statusbar), "demo");
+    // gtk_statusbar_push((GtkStatusbar *)w->statusbar,w->id,"Test");
+    //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
 
 
     gtk_widget_show_all (GTK_WIDGET (window));
@@ -112,9 +197,12 @@ static void activate (GtkApplication *app, gpointer user_data)
 
 int main (int argc, char **argv)
 {
-
+    srand((unsigned int)time(NULL));
 	int status;
     struct widget *d = g_malloc (sizeof (struct widget));
+    d->hard = false;
+    d->normal = true;
+    d->easy =  false;
 
 	d->app = gtk_application_new ("org.gtk.minimal", G_APPLICATION_FLAGS_NONE);
 	g_signal_connect (d->app, "activate", G_CALLBACK (activate), (gpointer) d);
